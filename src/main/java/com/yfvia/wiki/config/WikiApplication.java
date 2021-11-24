c29979c19843e9577721193fcf36e14c82a74366
yfviar <yfviar@163.com>
2021-11-24 11:12:09 +0800

    2.6 开发hello接口

--- src/main/java/com/yfvia/wiki/config/WikiApplication.java
+++ src/main/java/com/yfvia/wiki/config/WikiApplication.java
@@ -1,11 +1,12 @@
-package com.yfvia.wiki;
+package com.yfvia.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-
+@ComponentScan("com.yfvia.wiki")
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
---
+++ src/main/java/com/yfvia/wiki/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.yfvia.wiki.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "Hello via!";
+    }
+}
