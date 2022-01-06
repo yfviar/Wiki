51b473b806d42d6b8784f8418ee4ae9d2fce3650
yfviar <yfviar@163.com>
2022-01-06 19:05:12 +0800

    3.2  集成mybatis generator,生成demo代码

---
+++ doc/sql/all.sql
@@ -0,0 +1,9 @@
+drop table if exists `demo`;
+create table `demo`
+(
+    `id`   bigint not null comment 'id',
+    `name` varchar(50) comment '名称'
+)engine=innodb default charset=utf8mb4 comment='测试';
+
+insert into `demo` (id, name)
+values (1, '测试');
