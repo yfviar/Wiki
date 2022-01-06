a814fb7c1fd10347b663bcbf879fe686c2141218
yfviar <yfviar@163.com>
2022-01-06 11:58:58 +0800

    2.8 增加配置mybatis

--- src/main/java/com/yfvia/wiki/controller/TestController.java
+++ src/main/java/com/yfvia/wiki/controller/TestController.java
@@ -1,13 +1,18 @@
 package com.yfvia.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
 
+//    启动时，SpringBoot扫描到@Value注解，就回去配置文件读取配置信息，没读到就是默认值TEST
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello via!";
+        return testHello;
     }
 
     @PostMapping("/hello/post")
