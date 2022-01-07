2a109d18c950f05a36e94af03e925f63a079e38f
yfviar <yfviar@163.com>
2022-01-07 23:18:05 +0800

    4.6  制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    一木之禾
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+  name: "the-footer"
+}
+</script>
+
+<style scoped>
+
+</style>
