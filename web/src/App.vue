a4f86cb044c499090506d278e9f5e49acf2a695b
yfviar <yfviar@163.com>
2022-01-07 23:18:05 +0800

    4.6  制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,20 @@
   <a-layout>
     <the-header/>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      一木之禾
-    </a-layout-footer>
+    <the-footer/>
   </a-layout>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
 import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
 
 export default defineComponent({
   name: 'app',
   components: {
-    TheHeader
+    TheHeader,
+    TheFooter
   },
 });
 </script>
