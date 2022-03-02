e7d392b4ddc0edba2102c2ea8eceeb6defb164d9
yfviar <yfviar@163.com>
2022-03-02 11:50:35 +0800

    10.12 前后端增加登录拦截

--- src/main/java/com/yfvia/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/yfvia/wiki/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.yfvia.wiki.config;
 
+import com.yfvia.wiki.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -8,12 +9,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-//    @Autowired
-//    LogInterceptor logInterceptor;
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(logInterceptor)
-//                .addPathPatterns("/**").excludePathPatterns("/login");
-//    }
+    @Autowired
+    LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/login",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/find-content/**"
+
+                )
+        ;
+    }
 }
--- web/src/main.ts
+++ web/src/main.ts
@@ -6,15 +6,40 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 // import * as Icons from '@ant-design/icons-vue'
 import axios from "axios";
+import {Tool} from "@/util/tool";
 
 
 /**
  * axios拦截器
  */
-axios.interceptors.response.use(function (config){
+
+axios.interceptors.request.use(config => {
+        console.log('请求参数：', config);
+
+        const token = store.state.user.token;
+        if (Tool.isNotEmpty(token)) {
+            console.log("headers", config.headers);
+            let headers = JSON.stringify(config.headers);
+            headers = headers.substr(0, headers.length - 1) + ',"token":"' + token + '"}';
+            config.headers = JSON.parse(headers)
+
+            // console.log("headers", config.headers);
+            console.log("请求headers增加token:", token);
+
+        }
+        return config;
+    }
+    ,
+    error => {
+        return Promise.reject(error);
+    }
+)
+;
+
+axios.interceptors.response.use(function (config) {
     console.log('请求参数：', config);
     return config;
-},error => {
+}, error => {
     return Promise.reject(error);
 });
 
